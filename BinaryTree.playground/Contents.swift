61eaa86c65640782eac875c9907d6002f765d3b1
Tyler Poland <poland.tyler@gmail.com>
2019-06-10 22:46:12 -0500

    Swift binary tree playground

---
+++ BinaryTree.playground/Contents.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+class Node<T> {
+    var value: T
+    var leftChild: Node?
+    var rightChild: Node?
+    
+    init(value: T) {
+        self.value = value
+    }
+}
+
+enum BinaryTree<T> {
+    case empty
+    // node is recursive in this case, because it's calling itself
+    // without being marked indirect, the node *could* allocate memory for iteself indefinitely
+    // indirect introduces some reference semantics to this enum case (which would otherwise
+    // be a value type). (I think) Swift then lazy-loads more memory for the structure under the hood.
+    indirect case node(BinaryTree, T, BinaryTree)
+    
+    var count: Int {
+        switch self {
+        case let .node(left, _, right):
+            return left.count + 1 + right.count
+        case .empty:
+            return 0
+        }
+    }
+}
+
+
+/*
+ Modelling the following expression: (5 * (a - 10)) + (-4 * (3 / b))
+ */
+// leaf nodes
+let node5 = BinaryTree.node(.empty, "5", .empty)
+let nodeA = BinaryTree.node(.empty, "a", .empty)
+let node10 = BinaryTree.node(.empty, "10", .empty)
+let node4 = BinaryTree.node(.empty, "4", .empty)
+let node3 = BinaryTree.node(.empty, "3", .empty)
+let nodeB = BinaryTree.node(.empty, "b", .empty)
+
+// intermediate nodes on the left
+let aMinus10 = BinaryTree.node(nodeA, "-", node10)
+let timesLeft = BinaryTree.node(node5, "*", aMinus10)
+
+// intermediate nodes on the right
+let minus4 = BinaryTree.node(.empty, "-", node4)
+let divide3AndB = BinaryTree.node(node3, "/", nodeB)
+let timesRight = BinaryTree.node(minus4, "*", divide3AndB)
+
+// root node
+let tree = BinaryTree.node(timesLeft, "+", timesRight)
+
+extension BinaryTree: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case let .node(left, value, right):
+            return "value: \(value), left = [" + left.description + "], right = [" + right.description + "]"
+        case .empty:
+            return ""
+        }
+    }
+}
+
+print(tree)
+print(tree.count)
+
+// insertion into binary tree
+
