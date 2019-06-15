e1334b471396e2cd1df44298a763c37ce397c13a
Tyler Poland <poland.tyler@gmail.com>
2019-06-15 11:51:59 -0500

    BST insert javascript

--- BinaryTree.js
+++ BinaryTree.js
@@ -1,3 +1,6 @@
+// Disclaimer: I do not profess to be an expert (or even very knowledgeable)
+// with JavaScript, so please extend a bit of mercy :D - Tyler
+
 "use strict";
 
 class BinarySearchTree {
@@ -5,43 +8,47 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insertNumberNode(data, left = null, right = null) {
-    let Node = {
-      data,
-      left,
-      right
-    };
 
-    let currentNumberNode;
-    if (!this.root) {
-      // if not root, make it root by passing one
-      this.root = Node;
+
+// INSERTION
+  insert(data) {
+    var newNode = new Node(data);
+
+    if (this.root === null) {
+      node.left = newNode;
     } else {
-      currentNumberNode = this.root;
-      while (currentNumberNode) {
-        if (data < currentNumberNode.data) {
-          if (!currentNumberNode.left) {
-            currentNumberNode.left = Node;
-            break;
-          } else {
-
-          }
-        } else if (data > currentNumberNode.data) {
-          if (!currentNumberNode.right) {
-            currentNumberNode.right = Node;
-            break;
-          } else {
-            currentNumberNode = currentNumberNode.right;
-          }
-        } else {
-          console.log("Try a different value");
-          break;
-        }
-      }
+      tree.insertNode(this.root, newNode);
     }
   }
 
+  insertNode(node, newNode) {
+    // if data is less than Node
+    // data move to left of tree
+    if (newNode.data < node.data) {
 
+      // if left is null, insert here
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        // left is not null, call self recursively until null is found
+        this.insertNode(node.left, newNode);
+      }
+    }
+
+    // newNode data was greater than current node
+    // so data moves to right of tree
+    else {
+      // if the right side is null, insert here
+      if (node.right === null) {
+        node.right = newNode;
+      }
+
+      // right side was not null, call self recursively until null is found
+      else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
 
 
 }
